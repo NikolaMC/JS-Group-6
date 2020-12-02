efe7a9fe461b62ac877706c2d9c7899a19fb34b7
christoffermartell <69979256+christoffermartell@users.noreply.github.com>
2020-12-02 23:51:45 +0100

    Added Heading & changed color in Css

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-document.getElementById("test").innerHTML = "Hello";
+document.getElementById("head").innerHTML = "Ease Your Mind";
+document.querySelector("header").style.cssText = "color: black; font-size: 125px; font-family: courier;";
+
+document.getElementById("noteBooks").innerHTML = "Kategorisering?";
+document.querySelector("#noteBooks").style.cssText = "color: black; font-size: 30px; font-family: courier;";
