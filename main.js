309731a362c5acd70fa344866572365eaa1db1ea
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2020-12-06 15:11:00 +0100

    Added saveAsList function

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ document.getElementById("noteBooks").innerHTML = "Kategorisering?";
 document.querySelector("#noteBooks").style.cssText = "color: black; font-size: 30px; font-family: courier;";
 let inputForm = document.getElementById("inputForm");
 let inputArea = document.getElementById("inputArea");
-let inputSubmit = document.getElementById("inputSubmit");
+let inputSubmit = document.getElementById("inputText");
+let listSubmit = document.getElementById("inputList");
 let toggleButton = document.getElementById("toggleList");
 
 let main = document.getElementById("writeNotesHere");
 
-inputSubmit.addEventListener("click", function readForm(e) {
+inputSubmit.addEventListener("click", function saveAsText(e) {
     e.preventDefault();
 
     let newParagraph = document.createElement("p");
@@ -23,6 +24,27 @@ inputSubmit.addEventListener("click", function readForm(e) {
 
     inputArea.value = '';
 });
+
+listSubmit.addEventListener("click", function saveASList(e) {
+    e.preventDefault();
+
+    if (document.getElementById("ulElement") === null) {
+        let newUL = document.createElement("ul");
+        newUL.setAttribute("id", "ulElement");
+        main.appendChild(newUL);
+    }
+
+    let userText = inputArea.value;
+
+    let newLI = document.createElement("li");
+    let textNode = document.createTextNode(userText);
+    newLI.appendChild(textNode);
+
+    document.getElementById("ulElement").appendChild(newLI);
+
+    inputArea.value = '';
+})
+
  // supposed to be the localstorage function in main
 function storeInMain(){
     // still researching how to store correctly
