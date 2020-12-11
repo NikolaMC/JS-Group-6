bce0e8cd65bdfcaa05af2f849ce764735567e6aa
JoacimStahl96 <69908677+JoacimStahl96@users.noreply.github.com>
2020-12-11 16:21:31 +0100

    both list and normal text fixed in localstorage into main.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 document.getElementById("head").innerHTML = "Ease Your Mind";
 document.querySelector("header").style.cssText = "color: black; font-size: 125px; font-family: courier;";
 
-document.getElementById("noteBooks").innerHTML = "Kategorisering?";
+document.getElementById("noteBooks").innerHTML = "Sparat";
 document.querySelector("#noteBooks").style.cssText = "color: black; font-size: 30px; font-family: courier;";
 let inputForm = document.getElementById("inputForm");
 let inputArea = document.getElementById("inputArea");
@@ -14,6 +14,7 @@ let inputArray = window.localStorage.getItem("notes") ? JSON.parse(localStorage.
 let noteBookArray = window.localStorage.getItem("savedNoteBooks") ? JSON.parse(localStorage.getItem("savedNoteBooks")) : [];
 let listArray = window.localStorage.getItem("list") ? JSON.parse(localStorage.getItem("list")) : [];
 
+// makes the array elements into JSON strings which is required to store them into localstorage
 localStorage.setItem("notes", JSON.stringify(inputArray));
 localStorage.setItem("savedNoteBooks", JSON.stringify(noteBookArray));
 localStorage.setItem("list", JSON.stringify(listArray));
@@ -27,7 +28,7 @@ listSubmit.addEventListener("click", function saveASList(e) {
 	e.preventDefault();
 	saveList();
 });
-
+// saves the input as a normal text but also saves it in saveList() localstorage function
 function normalText() {
 	storeInMain();
 	let userText = inputArea.value;
@@ -38,7 +39,7 @@ function normalText() {
 
 	inputArea.value = "";
 }
-// saves the input as a list but also stores it in localStorage
+// saves the input as a list but also stores it in localStorage when it reaches storeListInMain()
 function saveList() {
 	if (document.getElementById("ulElement") === null) {
 		let newUL = document.createElement("ul");
@@ -59,12 +60,12 @@ function saveList() {
 	inputArea.value = "";
 }
 
-// supposed to be the localstorage function in main. plain text
+// Function to save plain text, normal notes
 function storeInMain() {
 	inputArray.push(inputArea.value);
 	localStorage.setItem("notes", JSON.stringify(inputArray));
 }
-// not finished function. store a list
+// Function to store the list elements.
 function storeListInMain() {
 	listArray.push(inputArea.value);
 	localStorage.setItem("list", JSON.stringify(listArray));
@@ -117,28 +118,42 @@ saveNoteList.addEventListener("click", function (e) {
 	let pList = document.getElementById("ulElement");
 
 	if (main.innerHTML == "") {
-		alert("Please enter something in the note before saving.");
+		alert("Please create a list before saving your list's.");
 	} else {
 		if (pList.textContent !== "") {
-			let newP2 = document.createElement("p");
+			let newP2 = document.createElement("li");
 			newP2.textContent = pList.textContent;
 			folder.appendChild(newP2);
 			saveInNoteBooks();
 			main.innerHTML = "";
-			console.log("added p");
-		} else {
-			console.log("Empty List");
 		}
 	}
 });
+// the arrays are changed into a normal javascript string before we call them
+// into the functions that show them on the page on refresh / opening of page.
 
-//document.getElementById("savedNoteBooks").innerHTML = JSON.parse(localStorage.getItem("noteBooks"));
-
-// the last one of the 3 above dominates what will be shown on the page.
 const savedNotesFromMain = JSON.parse(localStorage.getItem("notes"));
 const savedListFromMain = JSON.parse(localStorage.getItem("list"));
 const cache = JSON.parse(localStorage.getItem("savedNoteBooks"));
 
-document.getElementById("writeNotesHere").innerHTML = savedNotesFromMain + "," + savedListFromMain;
-document.getElementById("noteBooks").textContent = cache;
-console.log(cache);
+// the saved elements in the savedListFromMain gets restored to a UL on refresh
+function savedList() {
+	let todoList = document.createElement("ul");
+	for (let i = 0; i < savedListFromMain.length; i++) {
+		let child = document.createElement("li");
+		child.appendChild(document.createTextNode(savedListFromMain[i]));
+		todoList.appendChild(child);
+		document.getElementById("writeNotesHere").appendChild(todoList);
+	}
+}
+// the saved elements in savedNotesFromMain is again output as normal text on refresh.
+function savedNote() {
+	for (let i = 0; i < savedNotesFromMain.length; i++) {
+		let toDoNote = document.createElement("p");
+		toDoNote.appendChild(document.createTextNode(savedNotesFromMain[i]));
+		document.getElementById("writeNotesHere").appendChild(toDoNote);
+	}
+}
+// calls on the functions that has stored the elements so it's shown on opening of page / refresh
+savedNote();
+savedList();
