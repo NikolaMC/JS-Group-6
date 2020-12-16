3a8ba23e9469f5e534d21290533a58413a566702
Nikola Maljenovic <32510744+NikolaMC@users.noreply.github.com>
2020-12-16 10:41:44 +0100

    Removed footer

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+window.localStorage.clear();
 document.getElementById("head").innerHTML = "Ease Your Mind";
 document.querySelector("header").style.cssText = "color: black; font-size: 125px; font-family: courier;";
 
